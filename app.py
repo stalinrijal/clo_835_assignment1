5ae619f49f889f7993684068309037174b84d60e
Stalin Rijal <stalinrijal@gmail.com>
2024-09-30 21:39:10 -0400

    update and fix for db connection

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import argparse
 
 app = Flask(__name__)
 
-DBHOST = os.environ.get("DBHOST") or "mysql"
+DBHOST = os.environ.get("DBHOST") or "mysql-container"
 DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD") or "passwors"
+DBPWD = os.environ.get("DBPWD") or "password"
 DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT", 3306))  
